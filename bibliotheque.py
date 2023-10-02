5323ea73ae695c7746545feb357021663d0ed5ba
Biringanine Destin <113047575+DestinBir@users.noreply.github.com>
2023-10-02 14:47:22 +0200

    new file:   .idea/.gitignore 	new file:   .idea/Jupyter_Projects.iml 	new file:   .idea/inspectionProfiles/profiles_settings.xml 	new file:   .idea/misc.xml 	new file:   .idea/modules.xml 	new file:   .idea/workspace.xml 	new file:   .ipynb_checkpoints/ADS-checkpoint.ipynb 	new file:   .ipynb_checkpoints/Audio_Managing-checkpoint.ipynb 	new file:   .ipynb_checkpoints/Untitled-checkpoint.ipynb 	new file:   .ipynb_checkpoints/Untitled1-checkpoint.ipynb 	new file:   .ipynb_checkpoints/dddd.csv 	new file:   ".ipynb_checkpoints/g\303\251n\303\251rateur de faux fichiers-checkpoint.ipynb" 	new file:   ".ipynb_checkpoints/projet biblioth\303\250que -checkpoint.ipynb" 	new file:   ADS.ipynb 	new file:   Audio_Managing.ipynb 	new file:   Untitled.ipynb 	new file:   Untitled1.ipynb 	new file:   bibliotheque.py 	new file:   "g\303\251n\303\251rateur de faux fichiers.ipynb" 	new file:   "projet biblioth\303\250que .ipynb"

---
+++ bibliotheque.py
@@ -0,0 +1,12 @@
+
+
+
+class Book:
+
+
+    def __init__(self, title:str, img_path:str, book_path:str):
+        self.title = title
+        self.img_path = img_path
+        self.book_path = book_path
+
+    def get
